b715987fabbe63887a295a248b22a60a0ad135a2
Mikees23 <michael23.jd@gmail.com>
2018-05-07 18:03:40 +0600

    clear the deployed application

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Branch Week #1</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
